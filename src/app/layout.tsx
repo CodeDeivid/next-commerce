ec3e076f78cd057b0025a24b73a000939d6396ff
CodeDeivid <deividmickael@gmail.com>
2024-03-06 10:50:47 -0300

    Adicionando Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,26 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Navbar from './components/Navbar';
+import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Next E-Commerce 13',
-  description: 'Next E-Commerce utilizando a versão 13',
-}
+  title: "Next E-Commerce 13",
+  description: "Next E-Commerce utilizando a versão 13",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        <main className="bg-slate-700 h-screen p-16">{children}</main>
+      </body>
     </html>
-  )
+  );
 }
