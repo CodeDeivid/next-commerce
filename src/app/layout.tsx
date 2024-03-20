5576901b85034bbee5d52229a9895849b17c1562
CodeDeivid <deividmickael@gmail.com>
2024-03-20 18:45:45 -0300

    Página de detalhamento de Produtos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ptBR } from "@clerk/localizations";
-
+import Hydrate from "./components/Hydrate";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -22,8 +22,10 @@ export default function RootLayout({
     <ClerkProvider localization={ptBR}>
       <html lang="en">
         <body className={clsx(inter.className, "bg-slate-700")}>
-          <Navbar />
-          <main className="h-screen p-16">{children}</main>
+          <Hydrate>
+            <Navbar />
+            <main className="h-screen p-16">{children}</main>
+          </Hydrate>
         </body>
       </html>
     </ClerkProvider>
